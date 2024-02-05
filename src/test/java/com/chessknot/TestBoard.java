137d14c396b87fcbf051c15afd051e082186918f
l3002 <tanmaymathpal4545@gmail.com>
2024-02-05 23:08:39 +0530

    refactor: updated TestBoard for new file structure
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

--- src/test/java/com/chessknot/TestBoard.java
+++ src/test/java/com/chessknot/TestBoard.java
@@ -1,7 +1,7 @@
-package com.tests.chessknot.engine.tests;
+package com.chessknot;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -9,22 +9,21 @@ import com.chessknot.engine.board.Board;
 
 public class TestBoard {
 
-    @Test
-    public void intialBoard(){
-        final Board board = Board.createStandardBoard();
-        assertEquals(board.getCurrentPlayer().getLegalMoves().size(), 20);
-        assertEquals(board.getCurrentPlayer().getOpponent().getLegalMoves().size(), 20);
-        assertFalse(board.getCurrentPlayer().isInCheck());
-        assertFalse(board.getCurrentPlayer().isCheckmated());
-        assertFalse(board.getCurrentPlayer().isCastled());
-        assertEquals(board.getCurrentPlayer(), board.whitePlayer());
-        assertEquals(board.getCurrentPlayer().getOpponent(), board.blackPlayer());
-        assertFalse(board.getCurrentPlayer().getOpponent().isCastled());
-        assertFalse(board.getCurrentPlayer().getOpponent().isCheckmated());
-        assertFalse(board.getCurrentPlayer().getOpponent().isInCheck());
-        assertEquals(board.getCurrentPlayer().getActivePiece().size(), 16);
-        assertEquals(board.getCurrentPlayer().getOpponent().getActivePiece().size(), 16);
-    }
-    
-    
+  @Test
+  public void intialBoard() {
+    final Board board = Board.createStandardBoard();
+    assertEquals(board.getCurrentPlayer().getLegalMoves().size(), 20);
+    assertEquals(board.getCurrentPlayer().getOpponent().getLegalMoves().size(), 20);
+    assertFalse(board.getCurrentPlayer().isInCheck());
+    assertFalse(board.getCurrentPlayer().isCheckmated());
+    assertFalse(board.getCurrentPlayer().isCastled());
+    assertEquals(board.getCurrentPlayer(), board.whitePlayer());
+    assertEquals(board.getCurrentPlayer().getOpponent(), board.blackPlayer());
+    assertFalse(board.getCurrentPlayer().getOpponent().isCastled());
+    assertFalse(board.getCurrentPlayer().getOpponent().isCheckmated());
+    assertFalse(board.getCurrentPlayer().getOpponent().isInCheck());
+    assertEquals(board.getCurrentPlayer().getActivePiece().size(), 16);
+    assertEquals(board.getCurrentPlayer().getOpponent().getActivePiece().size(), 16);
+  }
+
 }
