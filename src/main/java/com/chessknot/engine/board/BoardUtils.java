7b03d032de717f94b4a84ffbe68fd5b6776f8f1c
l3002 <tanmaymathpal4545@gmail.com>
2024-02-06 23:03:27 +0530

    feat: added algebric notation and position-coordinate mapping
    Signed-off-by: l3002 <tanmaymathpal4545@gmail.com>

--- src/main/java/com/chessknot/engine/board/BoardUtils.java
+++ src/main/java/com/chessknot/engine/board/BoardUtils.java
@@ -1,5 +1,10 @@
 package com.chessknot.engine.board;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+
 public class BoardUtils {
     
     public static final boolean[] FIRST_COLUMN = initColumn(0);
@@ -16,11 +21,38 @@ public class BoardUtils {
     public static final boolean[] SECOND_RANK = initRow(48);
     public static final boolean[] FIRST_RANK = initRow(56);
 
+    public static final String[] ALGEBRIC_NOTATION = initializeAlgebricNotation();
+    public static final Map<String,Integer> POSITION_TO_COORDINATE = initializePositionToCoordinate();
+
     public static final int NUM_TILES = 64;
     public static final int NUM_TILES_PER_ROW = 8;
 
     private BoardUtils(){
         throw new RuntimeException("you cannot instantiate me!");
+    }
+
+    private static Map<String, Integer> initializePositionToCoordinate() {
+
+        final Map<String, Integer> positionToCoordinate = new HashMap<>();
+
+        for(int i = 0; i < NUM_TILES; i++){
+            positionToCoordinate.put(ALGEBRIC_NOTATION[i],i);
+        }
+        
+        return ImmutableMap.copyOf(positionToCoordinate);
+    }
+
+    private static String[] initializeAlgebricNotation() {
+        return new String[]{
+            "a8","b8","c8","d8","e8","f8","g8","h8",
+            "a7","b7","c7","d7","e7","f7","g7","h7",
+            "a6","b6","c6","d6","e6","f6","g6","h6",
+            "a5","b5","c5","d5","e5","f5","g5","h5",
+            "a4","b4","c4","d4","e4","f4","g4","h4",
+            "a3","b3","c3","d3","e3","f3","g3","h3",
+            "a2","b2","c2","d2","e2","f2","g2","h2",
+            "a1","b1","c1","d1","e1","f1","g1","h1"
+        };
     }
 
     private static boolean[] initRow(int initialTileCoordinate){
@@ -49,5 +81,13 @@ public class BoardUtils {
 
     public static boolean isValidTileCoordinate(final int coordinate){
         return coordinate >= 0 && coordinate < NUM_TILES;
+    }
+
+    public static int getCoordinateAtPosition(final String postion){
+        return POSITION_TO_COORDINATE.get(postion);
+    }
+
+    public static String getPositionAtCoordinate(final int coordinate) {
+        return ALGEBRIC_NOTATION[coordinate];
     }
 }
